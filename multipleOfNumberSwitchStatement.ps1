1503f639d3c80c8dfc3d1366458e587e4aa61a60
Tim <tmitchellmail@gmail.com>
2016-12-04 21:48:54 -0500

    multipleOfNumberSwitchStatements-v2
    Added more complex variations such as expanding the switch statement to
    display if the number is a multiple of more than one number and a
    foreach loop that can find if a number is a multiple of any number from
    1 to 100.

--- multipleOfNumberSwitchStatement.ps1
+++ multipleOfNumberSwitchStatement.ps1
@@ -2,8 +2,6 @@
 Author Tim Mitchell
 Date 12/4/2016
 Description: This script will print out the numbers 1 to 100, if the number is a multiple of 15, 5, or 3 it will say it.
-Note that the breaks at the end of each switch will prevent numbers from listing that they are mutliples from both 3, 5, and 15.
-If you want each number to say that they are a multiple of all up to all three of the switch statements then remove the semi colon and break.
 
 #>
 
@@ -20,3 +18,55 @@ Foreach ($number in 1..100)
         default { $_ }
         } 
     }
+
+
+
+#This version allows for a number to report if its a multiple of more than one of the numbers 15, 5, or 3. 
+Foreach ($number in 1..100) 
+    {
+    switch ($number) 
+        {
+        # In each switch condition if the number does not have a remainder then write the output of the current object $_ which is $number at the time of the loop then go to the next number from the break 
+        # else continue to the next condition until default.
+        {-not ($_ % 15 -or $_ % 5 -or $_ % 3 ) } {"$_ is multiple of 15, 5, and 3"; break }
+        {-not ($_ % 15 -or $_ % 5 ) } {"$_ is multiple of 15 and 5"; break }
+        {-not ($_ % 15 -or $_ % 3 ) } {"$_ is multiple of 15 and 3"; break }
+        {-not ($_ % 5 -or $_ % 3 ) } {"$_ is multiple of 5 and 3"; break }
+        {-not ($_ % 15 ) } {"$_ is multiple of 15"; break }
+        {-not ($_ % 5 ) } {"$_ is multiple of 5"; break }
+        {-not ($_ % 3) } {"$_ is multiple of 3"; break }
+        default { $_ }
+        } 
+    }
+
+
+
+# This version will get all the multiples of the numbers 1 to 100 using two foreach loops and an if statement
+
+# Run a foreach loop going through each number 1 through 100 and assigning it to a temporary variable $number
+Foreach ($number in 1..100) 
+    {
+    # Declare variable called $multiples as an array
+    $multiples = @()
+
+    # Run a foreach loop going through each number 1 through 100 and assigning it to a temporary variable $multiple to hold the multiple values
+    Foreach ($multiple in 1..100)
+    {
+        # If Statement that ensures that there is no remainder from dividing $number by $multiple then stores it in a temporary array $multiples
+        If (-not ($number % $multiple))
+        {
+            $multiples += $multiple
+
+        } # end if statement
+
+    } # End Foreach $multiple
+
+    # Output multiples of current $number
+    "$number is a multiple of $multiples"
+
+    # Delete the $mltiples array to reuse on next $number
+    Remove-Item variable:multiples
+
+} # End Foreach $number
+
+
